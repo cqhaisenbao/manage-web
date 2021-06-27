f6dc82bc868aa1189d82fdbfbe26a61cef5164f6
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-28 00:07:51 +0800

    基础样式与布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        home
-        <router-view></router-view>
+    <div class="basic-layout">
+        <aside></aside>
+        <main>
+            <nav>
+                <div class="bread">面包屑</div>
+                <div class="user">用户</div>
+            </nav>
+            <div class="wrapper">
+                <div class="main-page">
+                    <router-view></router-view>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
 
@@ -17,5 +27,41 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.basic-layout {
+    display: flex;
 
+    aside {
+        width: 200px;
+        height: 100vh;
+        background: #001529;
+        color: #fff;
+        overflow-y: auto;
+        flex-shrink: 0;
+        transition: width .25s;
+    }
+
+    main {
+        flex-grow: 1;
+
+        nav {
+            height: 50px;
+            line-height: 50px;
+            display: flex;
+            justify-content: space-between;
+            border-bottom: 1px solid #ddd;
+            padding: 0 20px;
+        }
+
+        .wrapper {
+            background: #eef0f3;
+            padding: 20px;
+            height: calc(100vh - 50px);
+
+            .main-page {
+                background: #fff;
+                height: 100%;
+            }
+        }
+    }
+}
 </style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="welcome">
         welcome
     </div>
 </template>
@@ -16,5 +16,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.welcome {
 
+}
 </style>
