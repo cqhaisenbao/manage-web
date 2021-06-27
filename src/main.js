8b4894e98514ef53a51464cf52e988bc7ad7967e
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-27 19:56:49 +0800

    封装axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from "./router";
 import Element from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css';
-import config from './config'
+import request from './utils/request'
 
 const app = createApp(App)
+//全局挂载
+app.config.globalProperties.$request = request
 app.use(router).use(Element).mount('#app')
---
+++ src/utils/request.js
@@ -0,0 +1,66 @@
+import axios from 'axios'
+import config from './../config'
+import {ElMessage} from 'element-plus'
+import router from "../router";
+
+const TOKEN_INVALID = 'Token认证失败，请重新登录'
+const NETWORK_ERROR = '网络请求异常'
+
+const service = axios.create({
+    baseURL: config.baseApi,
+    timeout: 8000,
+})
+
+service.interceptors.request.use((req) => {
+    const {headers} = req
+    if (!headers.Authorization) {
+        headers.Authorization = 'xx'
+    }
+    return req
+})
+
+service.interceptors.response.use((res) => {
+    const {code, data, msg = ''} = res.data
+    if (code === 200) {
+        return data
+    } else if (code === 40001) {
+        ElMessage.error(TOKEN_INVALID)
+        setTimeout(() => {
+            router.push('/login')
+        }, 1500)
+        return Promise.reject(TOKEN_INVALID)
+    } else {
+        ElMessage.error(msg || NETWORK_ERROR)
+        return Promise.reject(msg || NETWORK_ERROR)
+    }
+})
+
+function request(options) {
+    options.method = options.method || 'get'
+    if (options.method.toLowerCase() === 'get') {
+        options.params = options.data
+    }
+    if (config.env === 'prod') {
+        service.defaults.baseURL = config.baseApi
+    } else {
+        service.defaults.baseURL = config.mock ? config.mockApi : config.baseApi
+    }
+    return service(options)
+}
+
+// 另一种封装方法,通过request.get(url,data)调用
+['get', 'post', 'put', 'delete', 'patch'].forEach((item) => {
+    request[item] = (url, data, options) => {
+        return request({
+            url,
+            data,
+            method: item,
+            ...options
+        })
+    }
+})
+
+export default request
+
+
+
