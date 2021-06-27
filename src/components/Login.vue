627b6ef6d392991350832764d1af9c2ee5fb3514
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-27 19:55:54 +0800

    修改路由

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        login
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: "Login",
+    mounted() {
+        this.$request.get('/login').then(res => {
+            console.log(res);
+        });
+    },
+    setup() {
+        return {};
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from "../components/Home.vue";
 import Welcome from "../components/Welcome.vue";
+import Login from "../components/Login.vue";
 
 const routes = [
     {
@@ -13,9 +14,14 @@ const routes = [
         component: Home,
         children: [
             {
-                name: '/welcome',
+                name: 'welcome',
                 path: 'welcome',
                 component: Welcome
+            },
+            {
+                name: 'login',
+                path: 'login',
+                component: Login
             }
         ]
     }
