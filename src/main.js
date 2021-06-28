79fdd810e76bb0c51b620791073a655f73b2c37d
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-29 00:00:08 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import Element from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css';
 import request from './utils/request'
 import storage from './utils/storage'
+import api from './api'
+import store from './store'
 
 const app = createApp(App)
 //全局挂载
 app.config.globalProperties.$request = request
 app.config.globalProperties.$storage = storage
-app.use(router).use(Element).mount('#app')
+app.config.globalProperties.$api = api
+app.use(router).use(store).use(Element).mount('#app')
